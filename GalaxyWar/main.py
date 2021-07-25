e8bdbfd84015e27d7f5f8774e1f8d6aa2cb84630
Jkdar Jalal Naser <jakdarnaser@gmail.com>
2021-07-25 14:22:49 +0200

    new Game

--- GalaxyWar/main.py
+++ GalaxyWar/main.py
@@ -13,75 +13,79 @@ def gui():
 
 def getAllSpaceships():
     price = [
-        "-", 2500, 5000, 7500, 10000, 15000, 20000, 25000, 30000, 50000
+        1000, 2500, 5000, 7500, 10000, 15000
     ]
     lis = []
     player = basicPlayer()
     for i in range(6):
+        lis.append([])
         for j in range(7):
-            lis.append({
-                "name": "spaceship" + str(i),
+            bought = False
+            if j in player["spaceships"]["bought"][i]:
+                bought = True
+            lis[i].append({
+                "name": "spaceship-" + str(i) + "-" + str(j),
                 "img_src": "assets/spaceships/" + str(i) + "/" + str(j) + ".png",
                 "price": price[i],
                 "icon": "fa-coins",
-                "buyed": False
+                "bought": bought
             })
     return lis
 
 def getBullets():
     price = [
-        "-", 2500, 5000, 7500, 10000, 15000, 20000, 25000, 30000, 50000
+        1000, 2500, 5000, 7500, 10000, 15000, 20000, 25000, 30000, 50000
     ]
     lis = []
     player = basicPlayer()
     for i in range(10):
-        disabled = True
-        if i in player["bullets"]["buyed"]:
-            disabled = False
+        bought = False
+        if i in player["bullets"]["bought"]:
+            bought = True
         lis.append({
             "name": "bullet" + str(i),
             "img_src": "assets/bullets/red/0" + str(i) + ".png",
             "price": price[i],
             "icon": "fa-coins",
-            "buyed": disabled
+            "bought": bought
         })
     return lis
 
 def getFlames():
     price = [
-        "-", 2500, 5000, 7500, 10000, 15000, 20000, 25000, 30000, 50000
+        1000, 2500, 5000, 7500, 10000, 15000, 20000, 25000, 30000, 50000
     ]
     lis = []
     player = basicPlayer()
     for i in range(1):
-        disabled = True
-        if i in player["flames"]["buyed"]:
-            disabled = False
+        bought = False
+        if i in player["flames"]["bought"]:
+            bought = True
         lis.append({
             "name": "flames" + str(i),
             "img_src": "assets/flames/blue/0" + str(i) + ".png",
             "price": price[i],
             "icon": "fa-coins",
-            "buyed": disabled
+            "bought": bought
         })
     return lis
 
 def getBackgrounds():
     price = [
-        "-", 10000, 100000
+        1000, 10000, 100000
     ]
     lis = []
     player = basicPlayer()
     for i in range(3):
-        disabled = True
-        if i in player["backgrounds"]["buyed"]:
-            disabled = False
+        bought = False
+        if i in player["backgrounds"]["bought"]:
+            bought = True
         lis.append({
             "name": "background" + str(i),
             "img_src": "assets/backgrounds/" + str(i) + ".png",
             "price": price[i],
             "icon": "fa-coins",
-            "buyed": disabled
+            "bought": bought
         })
     return lis
 
@@ -89,38 +93,19 @@ def basicPlayer():
     player = {
         "coins": 1000,
         "spaceships": {
-            "buyed": [0, 1, 2, 3, 4, 5, 6]
+            "bought": [[0], [], [], [], [], []]
         },
         "bullets": {
-            "buyed": [0, 5, 9]
+            "bought": [0, 5, 9]
         },
         "flames": {
-            "buyed": [0]
+            "bought": [0]
         },
         "backgrounds": {
-            "buyed": [0, 1, 2]
+            "bought": [0, 1, 2]
         }
     }
     return player
-
-# def getSpaceships():
-#     price = [
-#         "-", 2500, 5000, 7500, 10000, 15000, 20000, 25000, 30000, 50000 
-#     ]
-#     lis = []
-#     player = basicPlayer()
-#     for i in range(7):
-#         disabled = True
-#         if i in player["spaceships"]["buyed"]:
-#             disabled = False
-#         lis.append({
-#             "name": "spaceship" + str(i),
-#             "img_src": "assets/spaceships/all/" + str(i) + ".png",
-#             "price": price[i],
-#             "icon": "fa-coins",
-#             "buyed": disabled
-#         })
-#     return lis
 
 if __name__ == "__main__":
     app.run()
